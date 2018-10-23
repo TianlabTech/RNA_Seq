4361817509f23b8526485ecf0f5d48a3e5b1925a
Tianlab技术小组 <43692442+TianlabTech@users.noreply.github.com>
2018-10-23 22:33:59 +0800

    Update 2_mkidx.sh

--- STAR_RSEM/2_mkidx.sh
+++ STAR_RSEM/2_mkidx.sh
@@ -3,10 +3,10 @@ perl get_iso.pl
 time STAR \
 --runThreadN 35 \
 --runMode genomeGenerate \
---genomeDir /local/pengying/data/anno/STAR \
+--genomeDir /local/pengying/data/anno/STAR \ #索引文件输出位置
 --genomeFastaFiles /local/pengying/data/anno/hg38.fa \
---sjdbGTFfile /local/pengying/data/anno/Homo_sapiens.GRCh38.87.chr.gtf \
---sjdbOverhang 100
+--sjdbGTFfile /local/pengying/data/anno/Homo_sapiens.GRCh38.87.chr.gtf \ #注意版本对应
+--sjdbOverhang 100 #理论取最大read长度-1，但是100效果也可以
 
 
 time /local/pengying/tools/RSEM-1.3.1/rsem-prepare-reference \
